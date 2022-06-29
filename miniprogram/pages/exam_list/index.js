d8c0fa9a04e8154d683142c4c2f155b068a91c11
Miv <787824374@qq.com>
2022-06-29 18:05:34 +0800

    考试之前需要登录的bug修复

--- miniprogram/pages/exam_list/index.js
+++ miniprogram/pages/exam_list/index.js
@@ -20,7 +20,23 @@ Page({
 			this.setData({tiList})
 		})
 	},
+	checkLogin(){
+		//判断用户登录
+		let userInfo = wx.getStorageSync('userInfo')
+		if(userInfo!=null && userInfo!=""){
+			return true
+		}else{
+			return false
+		}
+	},
 	goExam(e){
+		if(!this.checkLogin()){
+			wx.showToast({
+				title: '请先进行登录',
+				icon:'none'
+			})
+			return
+		}
 		let id = e.currentTarget.dataset.item._id
 		wx.navigateTo({
 			url: `/pages/exam/index?id=${id}`,
